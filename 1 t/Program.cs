1914f51cfaca946757f0158223c03c2ea748d76e
KET <varlamova_e.t@mail.ru>
2022-09-02 11:26:00 +0300

    1t

---
+++ 1 t/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+int result = 1;
+while(i < b)
+{
+   result = result*a;
+   i++;
+}
+Console.WriteLine($"Число А, возведенное в степень В равно: {result}");
+// Console.WriteLine(Math.Pow(a, b));
